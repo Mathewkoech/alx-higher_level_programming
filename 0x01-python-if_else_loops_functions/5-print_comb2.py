c1175969ed2fddae059ffc35f82915998c4ca49c
mathewkoech <mathewkoech55@gmail.com>
2024-01-02 12:42:16 -0800

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for number in range(0, 99):
-        print("{:02d}".format(number), end=", ")
-        print(number + 1)
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(99))
