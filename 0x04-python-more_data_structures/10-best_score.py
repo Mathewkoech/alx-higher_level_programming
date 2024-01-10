b875376dba91d0ebbe3662b88dc302862e615055
mathewkoech <mathewkoech55@gmail.com>
2024-01-10 04:41:48 -0800

    function that returns a list with all values multiplied by a number without using any loops.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,15 +3,15 @@
 def best_score(a_dictionary):
     """
     Returns the key with the biggest integer value in the dictionary.
-    
+
     Args:
         a_dictionary (dict): Input dictionary.
-    
-    Returns:
-        str: The key with the biggest integer value, or None if no score is found.
+
+   Returns:
+        str: The key with the biggest integer value, or None
     """
     if a_dictionary is None:
         return None
-    
+
     biggest_value_key = max(a_dictionary, key=lambda k: a_dictionary[k])
     return biggest_value_key
