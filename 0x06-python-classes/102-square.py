84a532c47a95551c35f47654ec18b1fc716272c3
mathewkoech <mathewkoech55@gmail.com>
2024-01-23 13:48:53 -0800

    a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """
+    Square class defines a square with a private instance attribute 'size'.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Size of square
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+        """defines area of a square"""
+
+    def area(self):
+        """
+        Area of the square
+        Return: squared size that is area
+        """
+        return self.__size**2
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != operator to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < operator to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= operator to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > operator to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= operator a Square."""
+        return self.area() >= other.area()
