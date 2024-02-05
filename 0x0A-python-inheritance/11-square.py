cf1f73e8ae1718cbe1386ed77caaba284a4f116c
mathewkoech <mathewkoech55@gmail.com>
2024-02-05 09:41:27 -0800

    square 1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a subclass of a rectangle"""
+    def __init__(self, size):
+        """instantiation of constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        """string rep"""
+        return "[square]" + str(self.__size ) + "/" + str(self.__size)
