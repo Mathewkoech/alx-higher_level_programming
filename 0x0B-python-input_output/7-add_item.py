098075cdf2f1014ad6a437dee7fe6269c13d83ea
mathewkoech <mathewkoech55@gmail.com>
2024-02-06 08:13:51 -0800

    function hat returns a list of lists of integers representing the Pascal’s triangle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A script that adds all arguments to a Python list,
-and then save them to a file:"""
+"""
+A script that adds all arguments to a Python list,
+and then save them to a file."""
 
 import sys
 
