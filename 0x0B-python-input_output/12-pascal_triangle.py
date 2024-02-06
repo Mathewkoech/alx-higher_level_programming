5114a43738b7184b5248cf9584eae7044f0b1359
mathewkoech <mathewkoech55@gmail.com>
2024-02-06 08:04:27 -0800

    function hat returns a list of lists of integers representing the Pascal’s triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,10 +11,12 @@ def pascal_triangle(n):
     triangle = []
 
     for i in range(n):
-        rows = []
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                prev_row = triangle[i - 1]
+                row.append(prev_row[j - 1] + prev_row[j])
         triangle.append(row)
-
-    return triangle
+    return (triangle)
