b5f4b2f3683980ce8aa8e3674bcb1233791381ac
mathewkoech <mathewkoech55@gmail.com>
2024-02-16 07:18:01 -0800

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -141,7 +141,6 @@ class Base:
         except IOError:
             return []
 
-
     @staticmethod
     def draw(list_rectangles, list_squares):
         """Draw Rectangles and Squares using the turtle module.
