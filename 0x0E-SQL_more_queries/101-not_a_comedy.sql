9417f6ef50cd6d2eabaf7795e5f7094069c8e29c
mathewkoech <mathewkoech55@gmail.com>
2024-03-13 06:57:25 -0700

    a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,6 @@
 -- lists all shows without comedy show in the database hbtn_0d_tv shows
 SELECT title FROM tv_shows
-WHERE NOT IN (
+WHERE title  NOT IN (
       SELECT title FROM tv_shows
       JOIN tv_show_genres ON tv_shows.id = show_id
       JOIN tv_genres ON genre_id = tv_genres.id WHERE `name` = "Comedy")
