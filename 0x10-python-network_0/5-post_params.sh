357cac2e3b4e649d71ce729f6972c687432e72e3
mathewkoech <mathewkoech55@gmail.com>
2024-04-25 03:00:00 -0700

    function that finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to the URL with the X-School-User-Id header and display the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
+#takes in a URL, sends a POST request to the passed URL,and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
