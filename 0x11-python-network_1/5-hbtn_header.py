30e46330f33e0176efa9bef2f2afb7f940e53ad2
mathewkoech <mathewkoech55@gmail.com>
2024-04-26 03:55:17 -0700

    script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the 
+Takes in a URL, sends a request to the URL and displays the
 value of the variable X-Request-Id in the response header
 Usage: ./5-hbtn_header.py https://alx-intranet.hbtn.io
 """
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     response = requests.get(url)
     body_response = response.headers.get('X-Request-Id')
     print(body_response)
-    
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a
+POST request to the passed URL with
+the email as a parameter, and displays the
+body of the response (decoded in utf-8)"""
+
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    # Extract the URL and email from command-line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email data
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    # Create a POST request
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
